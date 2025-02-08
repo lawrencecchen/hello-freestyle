acff2da17d9539b23bc62142bb7363f421dcb4f6
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-02-07 20:10:54 -0800

    wip

--- logs.ts
+++ logs.ts
@@ -4,6 +4,9 @@ const api = new FreestyleSandboxes({
   apiKey: process.env.FREESTYLE_API_KEY!,
 });
 
-api.getWebLogs("2df70dfe-1cf6-4c37-b470-c2ecaccb17af").then((logs) => {
-  console.log("Logs for project 2df70dfe-1cf6-4c37-b470-c2ecaccb17af: ", logs);
+const id = "767914ed-5c8a-4c2c-b05e-063570120840";
+// const id = "67606654-1a3f-41b3-bde1-34329c0a0b32";
+
+api.getWebLogs(id).then((logs) => {
+  console.log(`Logs for project ${id}:`, logs);
 });
